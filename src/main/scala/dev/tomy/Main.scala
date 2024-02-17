858712142a12caa543972cca8be441972d1fc641
Tomy <tomas.pinheiro.flores@gmail.com>
2024-02-17 23:26:59 +0000

    Updated Java version and added basic functionality

--- src/main/scala/dev/tomy/Main.scala
+++ src/main/scala/dev/tomy/Main.scala
@@ -4,14 +4,20 @@ import org.apache.spark.sql.SparkSession
 
 object Main {
   def main(args: Array[String]): Unit = {
+
+
     val spark = SparkSession.builder()
-      .master("local[1]")
-      .appName("XpandITDemo")
+      .master("local[*]")
+      .config("spark.driver.bindAddress","127.0.0.1")
+      .appName("XpandIT-demo")
       .getOrCreate()
 
-    println("Printing Spark Session Variables")
-    println("App name:" + spark.sparkContext.appName)
+    val df_1 = spark.read
+      .option("header", value = true)
+      .csv("datasets/googleplaystore.csv")
 
+    df_1.show()
+    df_1.printSchema()
     spark.stop()
   }
 }
