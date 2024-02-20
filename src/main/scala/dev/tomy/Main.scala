5b9caa24029e017a96d40dd80b7a6082ce2d3191
Tomy <tomas.pinheiro.flores@gmail.com>
2024-02-20 02:09:48 +0000

    SaveCSV

--- src/main/scala/dev/tomy/Main.scala
+++ src/main/scala/dev/tomy/Main.scala
@@ -2,7 +2,7 @@ package dev.tomy
 
 import java.util.Properties
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{avg, coalesce, col, lit}
+import org.apache.spark.sql.functions.{avg, coalesce, col, desc, lit}
 
 
 object Main {
@@ -14,20 +14,28 @@ object Main {
     properties.load(inputStream)
 
     //Creates Spark Session
-    val session = SparkSession.builder()
+    val sparkSession = SparkSession.builder()
       .master("local[*]")
       .config("spark.driver.bindAddress","127.0.0.1")
       .appName("XpandIT-demo")
       .getOrCreate()
 
+    val userReviews = Utils.loadCSV(sparkSession,properties.getProperty("app.userReviewsPath"))
+    val playStoreApps = Utils.loadCSV(sparkSession,properties.getProperty("app.playStoreAppsPath"))
 
     //Part 1
-    val userReviews = Utils.loadCSV(session,properties.getProperty("app.userReviewsPath"))
-    userReviews.show()
     //Converts "Sentiment_Polarity" column from String to Double and transforms NULL to 0
-    val castedUserReviews= userReviews.withColumn("Sentiment_Polarity", col("Sentiment_Polarity").cast("double")).na.fill(0.0)
-    val df_1 = castedUserReviews.groupBy("App").agg(avg("Sentiment_Polarity").as("Average_Sentiment_Polarity"))
+    ////val castedUserReviews= userReviews.withColumn("Sentiment_Polarity", col("Sentiment_Polarity").cast("double")).na.fill(0.0)
+    ////val df_1 = castedUserReviews.groupBy("App").agg(avg("Sentiment_Polarity").as("Average_Sentiment_Polarity"))
 
-    session.stop()
+    //Part 2
+
+    val filteredApps = playStoreApps.withColumn("Rating", coalesce(col("Rating"), lit(0)))
+      .filter(col("Rating") >= 4)
+      .filter(col("Rating") <= 5)
+
+    Utils.saveCSV(filteredApps,"outputs","df_2",",")
+
+    sparkSession.stop()
   }
 }
--- src/main/scala/dev/tomy/Utils.scala
+++ src/main/scala/dev/tomy/Utils.scala
@@ -8,12 +8,24 @@ object Utils {
   def loadCSV(session: SparkSession,pathToCSV: String): DataFrame = {
     val reviews = session.read
       .option("header", value = true)
-      /*Double quotes is an escape character and is used for quoting inside quoted value.
-       The option bellow allows it to be treated as regular character */
-      .option("escape","\"")
-      .csv("datasets/googleplaystore_user_reviews.csv")
+      .option("inferSchema", value = true)
+      .option("escape","\"") //Double quotes is an escape character and is used for quoting inside quoted value.This option allows it to be treated as regular character
+      .csv(pathToCSV)
 
     reviews
+  }
+
+  def saveCSV(df: DataFrame,outputPath: String, fileName: String, delimiter: String): Unit = {
+    df.write
+      .option("header", value = true)
+      .option("escape","\"")
+      .mode("overwrite")
+      .option("delimiter", delimiter)
+      .csv(s"$outputPath/$fileName")
+  }
+
+  def deleteExcessFiles(): Unit = {
+
   }
 
 
